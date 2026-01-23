bbe213ee688b6be769edc2f409d4b997c78c7f9c
Awaiskhan3573 <faraz.ali@markalytics.us>
2026-01-23 17:43:32 +0500

    started about page for desktop

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import HomeHero from '../components/home/HomeHero'
 
 const About = () => {
+  const texts = ["About Rmaac"];
   return (
-    <div>About</div>
+    <>
+      <h2 className='text-center py-5'>About Rmaac</h2>
+    </>
   )
 }
 
